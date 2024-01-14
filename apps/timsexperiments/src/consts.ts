d3359c1768a1e39606ce0d86e66dcc4be196a464
Tim <tim@timsexperiments.foo>
2024-01-14 16:24:53 -0600

    Update site title and description

--- apps/timsexperiments/src/consts.ts
+++ apps/timsexperiments/src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Tim's Experiments";
+export const SITE_DESCRIPTION =
+  'Check out some cool experiments about all things software engineering!';
 export const COLOR_PREFERENCE_KEY = 'timsexperiments:color_preference';
