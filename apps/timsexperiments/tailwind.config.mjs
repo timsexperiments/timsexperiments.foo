67af5780716a4edc2df16277a503989192b80d1b
Tim <tim@timsexperiments.foo>
2024-02-03 22:22:38 -0600

    Add swiping animation for MyJourney carousel on sm and xs screens

--- apps/timsexperiments/tailwind.config.mjs
+++ apps/timsexperiments/tailwind.config.mjs
@@ -77,10 +77,24 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        slide: {
+          '30%, 100%': {
+            opacity: 0,
+            transform: 'translateX(calc(1% * var(--slide-percent)))',
+          },
+          '0%': {
+            opacity: 0,
+            transform: 'translateX(calc(-1% * var(--slide-percent)))',
+          },
+          '10%': {
+            opacity: 1,
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        slide: 'slide 3s ease-in-out',
       },
     },
   },
