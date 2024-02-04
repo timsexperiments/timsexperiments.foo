35e5eb940b81e4ef0557df532ca0f33b2db83ecd
Tim <tim@timsexperiments.foo>
2024-02-03 22:25:09 -0600

    Add animation library to start animation when an element is in the viewport

---
+++ apps/timsexperiments/src/scripts/animate.ts
@@ -0,0 +1,28 @@
+const ANIMATE_IN_VIEWPORT_SELECTOR = '.animate-in-viewport';
+
+function setupAnimateStartInViewport() {
+  const observer = new IntersectionObserver((entries) => {
+    console.log('found:', entries);
+    entries.forEach((entry) => {
+      const target = entry.target as HTMLElement;
+      const animation = target.dataset.animation;
+      if (animation) {
+        target.classList.toggle(animation, entry.isIntersecting);
+      } else {
+        console.warn(
+          `Expected a "data-animation" property on ${target} but found none. ` +
+            'To set up start in viewport, the element should have a ' +
+            '"data-animation" property which specifies the animation to ' +
+            'start when the element enters the viewport.'
+        );
+      }
+    });
+  }, {});
+
+  const startInViewportEls = document.querySelectorAll<HTMLElement>(
+    ANIMATE_IN_VIEWPORT_SELECTOR
+  );
+  startInViewportEls.forEach((el) => observer.observe(el));
+}
+
+setupAnimateStartInViewport();
