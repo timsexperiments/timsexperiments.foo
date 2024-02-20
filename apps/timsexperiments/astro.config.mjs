9f5a3a505035bf499b0e04fc6031915c34821008
Tim <tim@timsexperiments.foo>
2024-02-19 23:01:38 -0600

    Fix app configuration

--- apps/timsexperiments/astro.config.mjs
+++ apps/timsexperiments/astro.config.mjs
@@ -1,9 +1,9 @@
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,4 +16,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  output: 'hybrid',
+  adapter: cloudflare({ imageService: 'passthrough' }),
 });
