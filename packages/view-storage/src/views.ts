ce5934669eefde734c4c6dbb53ec00fffa7d38fe
Tim <tim@timsexperiments.foo>
2024-02-23 20:02:36 -0600

    Add docstring comments

--- packages/view-storage/src/views.ts
+++ packages/view-storage/src/views.ts
@@ -17,10 +17,25 @@ export type FindViewsOptions = {
 export class ViewsStorage {
   constructor(private readonly db: ViewsDatabase) {}
 
+  /**
+   * Represents a ViewsStorage class.
+   *
+   * This class is responsible for storing views data using a ViewsDatabase.
+   *
+   * @param {ViewsDatabase} db - The ViewsDatabase instance used for storing views data.
+   */
   async add(...viewList: View[]) {
     return (await this.db.insert(views).values(viewList)).insertId;
   }
 
+  /**
+   * Retrieves the number of views for a specific page.
+   *
+   * This method queries the ViewsDatabase to retrieve the count of views for a given page.
+   *
+   * @param {string} page - The page for which to retrieve the view count.
+   * @returns {Promise<number>} - A promise that resolves to the number of views for the specified page.
+   */
   async pageViews(page: string) {
     return (
       await this.db
