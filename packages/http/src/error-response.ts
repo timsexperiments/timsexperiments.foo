b667a01286fe7f5a77896b64f23170648a3d5383
Tim <tim@timsexperiments.foo>
2024-02-23 21:52:09 -0600

    Update error responses to have an optional error message

--- packages/http/src/error-response.ts
+++ packages/http/src/error-response.ts
@@ -3,7 +3,7 @@ import { HttpStatuses } from "./status";
 /** Options for creating an HTTP response representing an HTTP error. */
 export interface ErrorResponseOptions {
   /** Message for the error response. */
-  message: string;
+  message?: string;
   /** The HTTP status code for the error. */
   status: number;
 }
@@ -11,29 +11,32 @@ export interface ErrorResponseOptions {
 /** Options for creating a not found error response. */
 export interface NotFoundResponseOptions {
   /** Message for the error response. */
-  message: string;
+  message?: string;
 }
 
 /**
  * Generates a response for a not found error.
  *
- * @param {NotFoundResponseOptions} options - The options for the not found response.
- * @param {string} options.message - The error message.
- * @returns {Response} The response object.
+ * @param options - The options for the not found response.
+ * @param options.message - The error message.
+ * @returns The response object.
  */
-export function notFoundResponse({ message }: NotFoundResponseOptions) {
+export function notFoundResponse(options?: Partial<NotFoundResponseOptions>) {
+  const { message } = options ?? {};
   return errorResponse({ message, status: HttpStatuses.STATUS_NOT_FOUND });
 }
 
 /**
  * Generates an error response with the given message and status.
  *
- * @param {ErrorResponseOptions} options - The options for the error response.
- * @param {string} options.message - The error message.
- * @param {number} options.status - The HTTP status code for the error response.
- * @returns {Response} - The error response.
+ * @param options - The options for the error response.
+ * @param options.message - The error message.
+ * @param options.status - The HTTP status code for the error response.
+ * @returns The error response.
  */
-export function errorResponse({ message, status }: ErrorResponseOptions) {
+export function errorResponse(options?: Partial<ErrorResponseOptions>) {
+  const { message, status = HttpStatuses.STATUS_INTERNAL_ERROR } =
+    options ?? {};
   return new Response(JSON.stringify({ message }), {
     headers: {
       "Content-Type": "application/json",
