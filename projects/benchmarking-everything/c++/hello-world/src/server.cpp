8761b8277911d8b33cdda86a490be74b34ad0943
Tim <tim@timsexperiments.foo>
2024-03-16 00:14:18 -0500

    Add c++ server

---
+++ projects/benchmarking-everything/c++/hello-world/src/server.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        cerr << "Error creating server socket" << endl;
+        return 1;
+    }
+
+    sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        cerr << "Error binding server socket to port 8080" << endl;
+        return 1;
+    }
+
+    if (listen(server_socket, 10) < 0) {
+        cerr << "Error listening on socket" << endl;
+        return 1;
+    }
+
+    while (true) {
+        sockaddr_in client_address;
+        socklen_t client_address_len = sizeof(client_address);
+        int client_socket = accept(server_socket, (sockaddr*)&client_address, &client_address_len);
+        if (client_socket < 0) {
+            cerr << "Error accepting incoming connection" << endl;
+            continue;
+        }
+
+        string responseString = "Hello World!";
+        string httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+                              + to_string(responseString.length()) + "\r\n\r\n" + responseString;
+
+        write(client_socket, httpResponse.c_str(), httpResponse.length());
+
+        close(client_socket);
+    }
+
+    close(server_socket);
+
+    return 0;
+}
