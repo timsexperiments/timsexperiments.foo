2709b144db5773962de3f360e6a0e155c9d09aa8
Tim <tim@timsexperiments.foo>
2024-03-15 23:47:48 -0500

    Add C# hello world with dockerfile

---
+++ projects/benchmarking-everything/c#/hello-world/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+HttpListener listener = new HttpListener();
+
+listener.Prefixes.Add("http://*:8080/");
+
+listener.Start();
+
+while (true)
+{
+    HttpListenerContext context = listener.GetContext();
+
+    HttpListenerResponse response = context.Response;
+
+    string responseString = "Hello World!";
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+    response.ContentType = "text/plain";
+
+    response.ContentLength64 = buffer.Length;
+
+    Stream output = response.OutputStream;
+    output.Write(buffer, 0, buffer.Length);
+
+    output.Close();
+}
