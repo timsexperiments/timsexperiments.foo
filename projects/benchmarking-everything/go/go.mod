07e98bc413edc23f155fe4019584c46e59366e3d
Tim <tim@timsexperiments.foo>
2024-03-17 18:50:49 -0500

    Add go helloworld

---
+++ projects/benchmarking-everything/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/timsexperiments/benchmarking-everything-go-hello-world
+
+go 1.21.7
---
+++ projects/benchmarking-everything/go/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
