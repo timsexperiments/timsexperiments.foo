98fc5b384dd0b43c27e9bdc702f810cd36701ecb
Tim <tim@timsexperiments.foo>
2024-03-17 19:50:42 -0500

    Add python hello_world

---
+++ projects/benchmarking-everything/python/hello_world/src/hello_world/main.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello World")
+
+    def log_message(self, format, *args):
+        # Override to prevent logging.
+        pass
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+    server_address = ("0.0.0.0", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Listening on port: {port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
