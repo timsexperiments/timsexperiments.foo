20ec8649d701b0c06223a9896196b843c19ff4ea
Tim <tim@timsexperiments.foo>
2024-03-17 20:47:39 -0500

    Update java helloworld to use executor threads

--- projects/benchmarking-everything/java/src/main/java/foo/timsexperiments/helloworld/HelloWorld.java
+++ projects/benchmarking-everything/java/src/main/java/foo/timsexperiments/helloworld/HelloWorld.java
@@ -4,21 +4,29 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class HelloWorld {
     public static void main(String[] args) throws IOException {
         int port = 8080;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server is listening on port " + port);
+        ExecutorService executor = Executors.newFixedThreadPool(12);
         try {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
 
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write(httpResponse.getBytes("UTF-8"));
-                outputStream.flush();
-                outputStream.close();
+                executor.submit(() -> {
+                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+                    try (OutputStream outputStream = clientSocket.getOutputStream()) {
+                        outputStream.write(httpResponse.getBytes("UTF-8"));
+                        outputStream.flush();
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
                 clientSocket.close();
             }
         } finally {
