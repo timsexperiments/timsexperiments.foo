b7ef5f29a51c3fa3976b67f1046b129ba69cf233
Tim <76897954+timsexperiments@users.noreply.github.com>
2024-03-19 13:48:52 +0000

    Replaced npm references with bun in all (non-.gitignore) files

--- apps/view-counter/src/index.ts
+++ apps/view-counter/src/index.ts
@@ -4,9 +4,9 @@ import { ViewsHandler } from './views';
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
- * - Run `npm run dev` in your terminal to start a development server
+ * - Run `bun run dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
+ * - Run `bun run deploy` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
